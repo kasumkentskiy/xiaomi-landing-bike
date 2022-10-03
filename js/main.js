f4b1bd41beee40bc374342a390e125a004ec34c1
willrock <114503576+kasumkentskiy@users.noreply.github.com>
2022-10-04 00:34:32 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+    $('.bike-slider, .slider__items').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+
+    
+    $(".menu, bike__column").on("click", "a", function (event) {
+        event.preventDefault();
+    
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+    
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+    
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+
+});
+
+
